e8ffdce5d77b4ad2a9d9faed91d6dd380b82d9cd
shubhamgit2003 <shubhamatanu@gmail.com>
2024-07-21 13:01:16 +0530

    2nd Commit

--- src/main/java/com/jbs/second_springbootv1/SecondSpringbootApplication.java
+++ src/main/java/com/jbs/second_springbootv1/SecondSpringbootApplication.java
@@ -9,6 +9,7 @@ public class SecondSpringbootApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SecondSpringbootApplication.class, args);
 		System.out.println("abcd");
+		System.out.println("Application started");
 	}
 
 }
